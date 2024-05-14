28067ab43068f017f90ae3f6103d749831b20ec4
Muhammad Samsuddin <samsmusa@outlook.com>
2024-05-15 00:13:18 +0600

    new update project status

--- src/components/CalenderFull/CalenderFull.tsx
+++ src/components/CalenderFull/CalenderFull.tsx
@@ -2,14 +2,14 @@
 
 import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
 import interactionPlugin from "@fullcalendar/interaction"; // needed for dayClick
-import { default as FullCalendar } from "@fullcalendar/react";
+import {default as FullCalendar} from "@fullcalendar/react";
 import {
-  CustomContentGenerator,
-  DateClickArg,
-  EventContentArg,
-  EventSourceInput,
+    CustomContentGenerator,
+    DateClickArg,
+    EventContentArg,
+    EventSourceInput,
 } from "fullcalendar/index.js";
-import { useRef, useState } from "react";
+import {useRef, useState} from "react";
 
 // const renderDayCellContent: CustomContentGenerator<DayCellContentArg> = (
 //   dayCellInfo,
@@ -20,56 +20,73 @@ import { useRef, useState } from "react";
 // };
 
 interface IEvent {
-  title: string;
-  date: string;
+    title: string;
+    date: string;
 }
 
 export const CalendarFull = () => {
-  const calendarRef = useRef(null);
-  const [data, setData] = useState<IEvent[]>([]);
-  const handleDateClick = (arg: DateClickArg) => {
-    alert(arg.dateStr);
-  };
-  const loadEvents: EventSourceInput = (
-    fetchInfo,
-    successCallback,
-    failureCallback,
-  ) => {
-    console.log("The Func is called");
-    successCallback([
-      { title: "Mr. Smith", date: "2024-05-01" },
-      { title: "Mr. Sanderheitkeith", date: "2024-05-04" },
-    ]);
-  };
-  return (
-    <FullCalendar
-      plugins={[dayGridPlugin, interactionPlugin]}
-      viewClassNames={""}
-      // dayCellClassNames={(arg) => {
-      //   // console.log("this is arg");
-      //   return `${arg.date === new Date("2024-05-04") ? "bg-red-400" : "bg-gray-100 "}  rounded-lg m-2 border-0`;
-      // }}
-      headerToolbar={{
-        right: "prev,next",
-        center: "title",
-        left: "today",
-      }}
-      eventTextColor="white"
-      themeSystem="Superhero"
-      dateClick={handleDateClick}
-      events={loadEvents}
-      eventContent={renderEventContent}
-    />
-  );
+    const calendarRef = useRef(null);
+    const [data, setData] = useState<IEvent[]>([]);
+
+    function goNext() {
+        if (calendarRef?.current) {
+            const calendarApi = calendarRef.current.getApi()
+            calendarApi.gotoDate(new Date(Date.UTC(2018, 8, 1)))
+        }
+
+    }
+
+    const handleDateClick = (arg: DateClickArg) => {
+        alert(arg.dateStr);
+    };
+    const loadEvents: EventSourceInput = (
+        fetchInfo,
+        successCallback,
+        failureCallback,
+    ) => {
+        console.log("The Func is called");
+        successCallback([
+            {title: "Mr. Smith", date: "2024-05-01"},
+            {title: "Mr. Sanderheitkeith", date: "2024-05-04"},
+        ]);
+    };
+    return (<>
+            <button onClick={goNext}>next</button>
+            <FullCalendar
+                // ref={calendarRef}
+                plugins={[
+                    dayGridPlugin,
+                    interactionPlugin
+                ]}
+                viewClassNames={"table-fixed"}
+                // dayCellClassNames={(arg) => {
+                //   // console.log("this is arg");
+                //   return `${arg.date === new Date("2024-05-04") ? "bg-red-400" : "bg-gray-100 "}  rounded-lg m-2 border-0`;
+                // }}
+                dayCellClassNames={"inline-block bg-brand w-[90px] m-1 rounded-3xl  h-[90px]"}
+                dayCellContent={''}
+                dayCellDidMount={()=>""}
+                headerToolbar={{
+                    right: "prev,next",
+                    center: "title",
+                    left: "today",
+                }}
+                eventTextColor="white"
+                themeSystem="Superhero"
+                dateClick={handleDateClick}
+                events={loadEvents}
+                eventContent={renderEventContent}
+            /></>
+    );
 };
 
 const renderEventContent: CustomContentGenerator<EventContentArg> = (
-  eventInfo,
+    eventInfo,
 ) => {
-  return (
-    <div className="bg-brand">
-      <b>{eventInfo.timeText}</b>
-      <i>{eventInfo.event.title}</i>
-    </div>
-  );
+    return (
+        <div className="bg-brand">
+            <b>{eventInfo.timeText}</b>
+            <i>{eventInfo.event.title}</i>
+        </div>
+    );
 };
